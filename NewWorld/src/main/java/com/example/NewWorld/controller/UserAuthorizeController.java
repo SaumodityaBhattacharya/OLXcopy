6f802639a579775b8380332aafd68c085af43d91
SaumodityaB <bhattacharyasaumodityo@gmail.com>
2026-06-11 18:15:07 +0530

    Added login feature and verification

--- NewWorld/src/main/java/com/example/NewWorld/controller/UserAuthorizeController.java
+++ NewWorld/src/main/java/com/example/NewWorld/controller/UserAuthorizeController.java
@@ -1,6 +1,7 @@
 package com.example.NewWorld.controller;
 
 import com.example.NewWorld.Exception.AgeVerificationException;
+import com.example.NewWorld.dto.UserLoginRequest;
 import com.example.NewWorld.entity.User;
 import com.example.NewWorld.service.UserService;
 import jakarta.validation.Valid;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.NewWorld.dto.AgeVerificationRequest;
 import java.time.LocalDate;
 import java.time.Period;
+import com.example.NewWorld.dto.UserLoginRequest;
 
 @RestController
 @RequestMapping("/UserAuthorize/api")
@@ -34,6 +36,10 @@ public class UserAuthorizeController {
             throw new AgeVerificationException("Seller must be 18 yrs old or above " +age);
         }
         return "Age verified successfully" +age;
+    }
+    @PostMapping("/userLogin")
+    public String LoginUser(@RequestBody UserLoginRequest request){
+        return userService.userLogin(request);
     }
 
 }
---
+++ NewWorld/src/main/java/com/example/NewWorld/dto/UserLoginRequest.java
@@ -0,0 +1,33 @@
+package com.example.NewWorld.dto;
+
+public class UserLoginRequest {
+
+    private String userEmail;
+    private String userPhoneNumber;
+    private String userPassword;
+    public UserLoginRequest(){}
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserPhoneNumber() {
+        return userPhoneNumber;
+    }
+
+    public void setUserPhoneNumber(String userPhoneNumber) {
+        this.userPhoneNumber = userPhoneNumber;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+}
